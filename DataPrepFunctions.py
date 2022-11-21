2fc918aba2b1cdb8e83aae65d469c29e847adde0
aeusepi <eusepi.alessandro@gmail.com>
2022-11-21 15:29:14 +0000

    Afternoon Commit

--- DataPrepFunctions.py
+++ DataPrepFunctions.py
@@ -87,7 +87,7 @@ def clean_data_basic(df,col2exclude):
     cat_vars = df.select_dtypes(include=['object']).copy().columns
     for var in  cat_vars:
         # for each cat add dummy var, drop original column
-        df = pd.concat([df.drop(var, axis=1), pd.get_dummies(df[var], prefix=var, prefix_sep='_', drop_first=True)], axis=1)
+        df = pd.concat([df.drop(var, axis=1), pd.get_dummies(df[var], prefix=var, prefix_sep='_', drop_first=False)], axis=1)
     
     X = df
     return X
